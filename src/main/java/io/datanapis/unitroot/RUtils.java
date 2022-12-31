24d6c10ef0047380dae24455d25b6e40a0b3789e
Jayakumar Muthukumarasamy <jk@macenters.org>
2022-12-31 06:58:00 -0800

    Simplify evaluator code while fixing a bug. Specifically, the previous implementation did not include lagged values as predictors.

---
+++ src/main/java/io/datanapis/unitroot/RUtils.java
@@ -0,0 +1,72 @@
+package io.datanapis.unitroot;
+
+public class RUtils {
+    /**
+     * Return lagged difference of x. The first value will be x[lag] - x[0], the second value will be x[1+lag] - x[1]
+     * and the i'th value will be x[i+lag] - x[i].
+     *
+     * @param x the values to lag
+     * @param lag the lag
+     * @return the lagged values. The return array will be of size x.length - lag
+     */
+    public static double[] diff(double[] x, int lag) {
+        int length = x.length;
+        if (lag <= 0 || lag >= length)
+            throw new IllegalArgumentException();
+
+        double[] y = new double [x.length - lag];
+        for (int i = 0; i < y.length; i++) {
+            y[i] = x[i+lag] - x[i];
+        }
+
+        return y;
+    }
+
+    /**
+     * See R function embed for documentation
+     *
+     * @param y the values to embed
+     * @param dimension the dimensions
+     * @return a two-dimensional matrix consisting of 'dimension' columns each with lagged values of y starting from
+     * dimension - 1 (0th column) till 0 (last column). The column values are stored sequentially. Each column is of
+     * length y.length - dimension + 1 (since the 0th column will only have y.length - dimension + 1 valid values)
+     */
+    public static double[] embed(double[] y, int dimension) {
+        int length = y.length;
+        if (dimension <= 0 || dimension >= length)
+            throw new IllegalArgumentException();
+
+        int size = length - dimension + 1;
+        double[] value = new double [dimension * size];
+        for (int i = 0; i < dimension; i++) {
+            System.arraycopy(y, dimension - i - 1, value, i * size, size);
+        }
+
+        return value;
+    }
+
+    /**
+     * Return values for 'column' from a two-dimensional matrix y. Each column is expected to be stored
+     * sequentially within y and is of size 'length'
+     *
+     * @param y the two-dimensional matrix with column values stored sequentially
+     * @param column the column whose value needs to be returned
+     * @param length the length of each column
+     * @return the values for column
+     */
+    public static double[] column(double[] y, int column, int length) {
+        double[] c = new double [length];
+        System.arraycopy(y, column * length, c, 0, length);
+        return c;
+    }
+
+    public static void copyColumn(double[] src, int i, double[] dest, int j, int length) {
+        System.arraycopy(src, i * length, dest, j * length, length);
+    }
+
+    public static void copyColumnTransposed(double[] src, int i, double[] dest, int j, int nCols, int length) {
+        for (int k = 0; k < length; k++) {
+            dest[k * nCols + j] = src[i * length + k];
+        }
+    }
+}
--- src/main/java/io/datanapis/unitroot/UnitRootEvaluator.java
+++ src/main/java/io/datanapis/unitroot/UnitRootEvaluator.java
@@ -6,6 +6,8 @@ import io.datanapis.unitroot.distribution.TestType;
 import io.datanapis.unitroot.distribution.UrcDist;
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 
+import java.util.Arrays;
+
 
 public class UnitRootEvaluator {
     private static final int NP = 9;
@@ -52,65 +54,6 @@ public class UnitRootEvaluator {
     }
 
     /**
-     * Return lagged difference of x. The first value will be x[lag] - x[0], the second value will be x[1+lag] - x[1]
-     * and the i'th value will be x[i+lag] - x[i].
-     *
-     * @param x the values to lag
-     * @param lag the lag
-     * @return the lagged values. The return array will be of size x.length - lag
-     */
-    private static double[] diff(double[] x, int lag) {
-        int length = x.length;
-        if (lag <= 0 || lag >= length)
-            throw new IllegalArgumentException();
-
-        double[] y = new double [x.length - lag];
-        for (int i = 0; i < y.length; i++) {
-            y[i] = x[i+lag] - x[i];
-        }
-
-        return y;
-    }
-
-    /**
-     * See R function embed for documentation
-     *
-     * @param y the values to embed
-     * @param dimension the dimensions
-     * @return a two-dimensional matrix consisting of 'dimension' columns each with lagged values of y starting from
-     * dimension - 1 (0th column) till 0 (last column). The column values are stored sequentially. Each column is of
-     * length y.length - dimension + 1 (since the 0th column will only have y.length - dimension + 1 valid values)
-     */
-    private static double[] embed(double[] y, int dimension) {
-        int length = y.length;
-        if (dimension <= 0 || dimension >= length)
-            throw new IllegalArgumentException();
-
-        int size = length - dimension + 1;
-        double[] value = new double [dimension * size];
-        for (int i = 0; i < dimension; i++) {
-            System.arraycopy(y, dimension - i - 1, value, i * size, size);
-        }
-
-        return value;
-    }
-
-    /**
-     * Return values for 'column' from a two-dimensional matrix y. Each column is expected to be stored
-     * sequentially within y and is of size 'length'
-     *
-     * @param y the two-dimensional matrix with column values stored sequentially
-     * @param column the column whose value needs to be returned
-     * @param length the length of each column
-     * @return the values for column
-     */
-    private static double[] column(double[] y, int column, int length) {
-        double[] c = new double [length];
-        System.arraycopy(y, column * length, c, 0, length);
-        return c;
-    }
-
-    /**
      * This is a port of the function unitrootTest from the R package fUnitRoot (file UnitRootTests.R)
      *
      * @return an instance of UnitRootResult containing the coefficients (0th index is the constant term if Type != NC),
@@ -121,12 +64,9 @@ public class UnitRootEvaluator {
 
         /* Δy(t) = λy(t−1) + μ + βt + α(1)Δy(t−1) + ... + α(k)Δy(t−k) + ε(t) */
 
-        double[] y = diff(x, 1);    /* get differences between consecutive terms */
+        double[] y = RUtils.diff(x, 1);    /* get differences between consecutive terms */
         int n = y.length;
-        double[] z = embed(y, lag);     /* See R function embed, logic from UnitrootTests.R from fUnitRoots */
-
-        /* 0th column becomes the response variable */
-        double[] yDiff = column(z, 0, z.length / lag);
+        double[] z = RUtils.embed(y, lag);     /* See R function embed, logic from UnitrootTests.R from fUnitRoots */
 
         /* the lagged original values become the first predictor */
         double[] yLag1 = new double [n - lag + 1];
@@ -134,51 +74,40 @@ public class UnitRootEvaluator {
 
         assert yLag1.length == (z.length / lag);
 
-        int offset = 1;
         int nCols = lag + 1;   /* +1 for the response variable - see OLSMultipleLinearRegression for details */
         if (this.type == Type.CONSTANT_WITH_TREND) {
             /* Need to add a lag term as a predictor */
             nCols += 1;
-            offset += 1;
         } else if (this.type == Type.CONSTANT_WITH_TREND_SQUARED) {
             /* Need to add both a lag and lag**2 as predictors */
             nCols += 2;
-            offset += 2;
         }
 
-        int tt = lag;
-
         double[] values = new double [nCols * yLag1.length];
-        for (int i = 0; i < yLag1.length; i++) {
-            for (int j = 0; j < nCols; j++) {
-                switch (j) {
-                    case 0:
-                        /* response values for regression, i.e. difference in y values, see equation above */
-                        values[i * nCols] = yDiff[i];
-                        continue;
-                    case 1:
-                        /* first predictor, i.e. lagged y, see equation above */
-                        values[i * nCols + 1] = yLag1[i];
-                        continue;
-                    case 2:
-                        if (this.type == Type.CONSTANT_WITH_TREND) {
-                            /* lag becomes a predictor */
-                            values[i * nCols + j] = tt;
-                            ++tt;
-                            continue;
-                        } else if (this.type == Type.CONSTANT_WITH_TREND_SQUARED) {
-                            /* both lag and lag**2 become predictors */
-                            values[i * nCols + j] = tt;
-                            j++;
-                            values[i * nCols + j] = tt * tt;
-                            ++tt;
-                            continue;
-                        }
-                    default:
-                        /* columns, 1 through lag are used as predictors as well */
-                        values[i * nCols + j] = z[(j - offset) * yLag1.length + i];
-                }
+        int j = 0;
+        /* response values for regression, 0th column becomes the response variable, see equation above */
+        RUtils.copyColumnTransposed(z, 0, values, j, nCols, yLag1.length); ++j;
+        /* first predictor, i.e. lagged y, see equation above */
+        RUtils.copyColumnTransposed(yLag1, 0, values, j, nCols, yLag1.length); ++j;
+        if (this.type == Type.CONSTANT_WITH_TREND || this.type == Type.CONSTANT_WITH_TREND_SQUARED) {
+            double[] tt = new double [yLag1.length];
+            for (int i = 0; i < yLag1.length; i++) {
+                tt[i] = lag + i;
             }
+            /* lag becomes a predictor */
+            RUtils.copyColumnTransposed(tt, 0, values, j, nCols, yLag1.length); ++j;
+            if (this.type == Type.CONSTANT_WITH_TREND_SQUARED) {
+                double[] ttSquared = new double [yLag1.length];
+                for (int i = 0; i < yLag1.length; i++) {
+                    ttSquared[i] = Math.pow(lag + i, 2.0);
+                }
+                /* lag**2 becomes a predictor as well */
+                RUtils.copyColumnTransposed(ttSquared, 0, values, j, nCols, yLag1.length); ++j;
+            }
+        }
+        /* columns, 1 through lag are used as predictors as well, 0'th column is the response variable */
+        for (int i = 1; i < lag; i++) {
+            RUtils.copyColumnTransposed(z, i, values, j, nCols, yLag1.length); ++j;
         }
 
         try {
